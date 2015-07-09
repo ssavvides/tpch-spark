17815c8ae979eadbc1f09f82c425e61f1417a8a0
Savvas Savvides <savvas@purdue.edu>
2015-07-09 16:14:19 -0400

    added spark tpc-h Q05

---
+++ src/main/scala/Q05.scala
@@ -0,0 +1,37 @@
+package main.scala
+
+import org.apache.spark.sql.functions.sum
+import org.apache.spark.sql.functions.udf
+
+/**
+ * TPC-H Query 5
+ * Savvas Savvides <ssavvides@us.ibm.com>
+ *
+ */
+class Q05 extends TpchQuery {
+
+  import sqlContext.implicits._
+
+  override def execute(): Unit = {
+
+    val decrease = udf { (x: Double, y: Double) => x * (1 - y) }
+
+    val forders = order.filter($"o_orderdate" < "1995-01-01" && $"o_orderdate" >= "1994-01-01")
+
+    val res = region.filter($"r_name" === "ASIA")
+      .join(nation, $"r_regionkey" === nation("n_regionkey"))
+      .join(supplier, $"n_nationkey" === supplier("s_nationkey"))
+      .join(lineitem, $"s_suppkey" === lineitem("l_suppkey"))
+      .select($"n_name", $"l_extendedprice", $"l_discount", $"l_orderkey", $"s_nationkey")
+      .join(forders, $"l_orderkey" === forders("o_orderkey"))
+      .join(customer, $"o_custkey" === customer("c_custkey") && $"s_nationkey" === customer("c_nationkey"))
+      .select($"n_name", decrease($"l_extendedprice", $"l_discount").as("value"))
+      .groupBy($"n_name")
+      .agg(sum($"value").as("revenue"))
+      .sort($"revenue".desc)
+
+    res.collect().foreach(println)
+
+  }
+
+}
