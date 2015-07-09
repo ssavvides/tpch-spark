c3696a44a5c122ef0b140e807c1445ee7deb2b9b
Savvas Savvides <savvas@purdue.edu>
2015-07-09 16:35:33 -0400

    added spark tpc-h query 6

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 TPC-H queries in spark
 
 Savvas Savvides
+
 ssavvides@us.ibm.com
+
 savvas@purdue.edu
 
 Data generator from:
+
 => TPC-H DBGEN (http://www.tpc.org/tpch/)
---
+++ src/main/scala/Q06.scala
@@ -0,0 +1,24 @@
+package main.scala
+
+import org.apache.spark.sql.functions.sum
+import org.apache.spark.sql.functions.udf
+
+/**
+ * TPC-H Query 6
+ * Savvas Savvides <ssavvides@us.ibm.com>
+ *
+ */
+class Q06 extends TpchQuery {
+
+  import sqlContext.implicits._
+
+  override def execute(): Unit = {
+
+    val res = lineitem.filter($"l_shipdate" >= "1994-01-01" && $"l_shipdate" < "1995-01-01" && $"l_discount" >= 0.05 && $"l_discount" <= 0.07 && $"l_quantity" < 24)
+      .agg(sum($"l_extendedprice" * $"l_discount"))
+
+    res.collect().foreach(println)
+
+  }
+
+}
