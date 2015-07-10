211510931c3b5fed10c6f9859d08b775b9998faf
Savvas Savvides <savvas@purdue.edu>
2015-07-10 11:22:25 -0400

    tpc-h query 10

--- src/main/scala/Q10.scala
+++ src/main/scala/Q10.scala
@@ -14,23 +14,21 @@ class Q10 extends TpchQuery {
 
   override def execute(): Unit = {
 
-    val getYear = udf { (x: String) => x.substring(0, 4) }
-    val expr = udf { (x: Double, y: Double, v: Double, w: Double) => x * (1 - y) - (v * w) }
+    val decrease = udf { (x: Double, y: Double) => x * (1 - y) }
 
-    val linePart = part.filter($"p_name".contains("green"))
-      .join(lineitem, $"p_partkey" === lineitem("l_partkey"))
+    val flineitem = lineitem.filter($"l_returnflag" === "R")
 
-    val natSup = nation.join(supplier, $"n_nationkey" === supplier("s_nationkey"))
-
-    val res = linePart.join(natSup, $"l_suppkey" === natSup("s_suppkey"))
-      .join(partsupp, $"l_suppkey" === partsupp("ps_suppkey")
-        && $"l_partkey" === partsupp("ps_partkey"))
-      .join(order, $"l_orderkey" === order("o_orderkey"))
-      .select($"n_name", getYear($"o_orderdate").as("o_year"),
-        expr($"l_extendedprice", $"l_discount", $"ps_supplycost", $"l_quantity").as("amount"))
-      .groupBy($"n_name", $"o_year")
-      .agg(sum($"amount"))
-      .sort($"n_name", $"o_year".desc)
+    val res = order.filter($"o_orderdate" < "1994-01-01" && $"o_orderdate" >= "1993-10-01")
+      .join(customer, $"o_custkey" === customer("c_custkey"))
+      .join(nation, $"c_nationkey" === nation("n_nationkey"))
+      .join(flineitem, $"o_orderkey" === flineitem("l_orderkey"))
+      .select($"c_custkey", $"c_name",
+        decrease($"l_extendedprice", $"l_discount").as("volume"),
+        $"c_acctbal", $"n_name", $"c_address", $"c_phone", $"c_comment")
+      .groupBy($"c_custkey", $"c_name", $"c_acctbal", $"c_phone", $"n_name", $"c_address", $"c_comment")
+      .agg(sum($"volume").as("revenue"))
+      .sort($"revenue".desc)
+      .limit(20)
 
     res.collect().foreach(println)
 
